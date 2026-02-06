9e8c382a0eacf94000407757a7c9888188e0b07f
rickviper <ullasroy05@gmail.com>
2026-02-06 19:49:41 +0530

    add readme.md file

--- app/images.py
+++ app/images.py
@@ -7,5 +7,5 @@ load_dotenv()
 imagekit = ImageKit(
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
     public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
-    url_endpoint=os.getenv("IMAGEKIT_URL"),
+    url_endpoint=os.getenv("IMAGEKIT_URL")
 )
